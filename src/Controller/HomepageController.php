b2af73cde97b137086d402f5620c5ec4ab69ccc3
KevinVanphavong <68031837+KevinVanphavong@users.noreply.github.com>
2022-11-28 23:44:07 +0100

    create template for skillset exp view and change navbar style

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,8 +11,32 @@ class HomepageController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function indexHomepage(): Response
     {
         return $this->render('homepage.html.twig');
+    }
+
+    /**
+     * @Route("/about-me", name="about-me")
+     */
+    public function indexAboutMe(): Response
+    {
+        return $this->render('about-me.html.twig');
+    }
+
+    /**
+     * @Route("/hbo-studio", name="hbo-studio")
+     */
+    public function indexHboStudio(): Response
+    {
+        return $this->render('hbo-studio.html.twig');
+    }
+
+    /**
+     * @Route("/skillset-experiences", name="skillset-experiences")
+     */
+    public function indexSkillsetExperiences(): Response
+    {
+        return $this->render('skillset-experiences.html.twig');
     }
 }
